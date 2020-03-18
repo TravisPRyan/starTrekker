155943f3b1f3071efdfd4ff0bff6f253bb207e57
TravisPRyan <TravisPRyan@protonmail.com>
2020-03-18 13:41:58 -0400

    v1 File Org, Routes, Pages

---
+++ src/destinations/pages/NewDest.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewDest = () => {
+  return <h1> New Destination Works</h1>;
+};
+
+export default NewDest;
---
+++ src/trekker/pages/Trekkers.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Trekkers = () => {
+  return <h1>Trekkers Page works!</h1>;
+};
+
+export default Trekkers;
