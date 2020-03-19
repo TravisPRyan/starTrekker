66441b23f1161da80698a7a09ad51b7951939eb7
TravisPRyan <TravisPRyan@protonmail.com>
2020-03-19 14:46:59 -0400

    v3 Preliminary Styles

--- src/trekker/pages/Trekkers.js
+++ src/trekker/pages/Trekkers.js
@@ -1,11 +1,12 @@
 import React from "react";
 import TrekkersList from "../components/TrekkersList";
+import "../../App.css";
 
 const Trekkers = () => {
   const TREKKERS = [
     {
-      id: "1",
-      name: "Fred Smith",
+      id: "u1",
+      name: "Frederick Smith",
       image:
         "https://media3.s-nbcnews.com/j/newscms/2019_41/3047866/191010-japan-stalker-mc-1121_06b4c20bbf96a51dc8663f334404a899.fit-1000w.JPG",
       destinations: "3"
