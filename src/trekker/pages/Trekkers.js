7eab29c8191bba54562c256a0595243a3ce08619
TravisPRyan <TravisPRyan@protonmail.com>
2020-03-24 16:25:03 -0400

    v4 Styles Cont.

--- src/trekker/pages/Trekkers.js
+++ src/trekker/pages/Trekkers.js
@@ -1,12 +1,32 @@
 import React from "react";
 import TrekkersList from "../components/TrekkersList";
-import "../../App.css";
 
 const Trekkers = () => {
   const TREKKERS = [
     {
       id: "u1",
       name: "Frederick Smith",
+      image:
+        "https://media3.s-nbcnews.com/j/newscms/2019_41/3047866/191010-japan-stalker-mc-1121_06b4c20bbf96a51dc8663f334404a899.fit-1000w.JPG",
+      destinations: "3"
+    },
+    {
+      id: "u1",
+      name: "Robert Grace",
+      image:
+        "https://media3.s-nbcnews.com/j/newscms/2019_41/3047866/191010-japan-stalker-mc-1121_06b4c20bbf96a51dc8663f334404a899.fit-1000w.JPG",
+      destinations: "3"
+    },
+    {
+      id: "u1",
+      name: "Candice Smith",
+      image:
+        "https://media3.s-nbcnews.com/j/newscms/2019_41/3047866/191010-japan-stalker-mc-1121_06b4c20bbf96a51dc8663f334404a899.fit-1000w.JPG",
+      destinations: "3"
+    },
+    {
+      id: "u1",
+      name: "Meredith Grace",
       image:
         "https://media3.s-nbcnews.com/j/newscms/2019_41/3047866/191010-japan-stalker-mc-1121_06b4c20bbf96a51dc8663f334404a899.fit-1000w.JPG",
       destinations: "3"
