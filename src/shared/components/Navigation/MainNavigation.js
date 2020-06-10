87791b2702fd95a9e12fecfb64e69674a2683e2c
TravisPRyan <TravisPRyan@protonmail.com>
2020-06-10 13:33:53 -0400

    v17 SideDrawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
 import SideDrawer from "./SideDrawer";
-import "./MainNavigation.css";
 import NavLinks from "./NavLinks";
+import "./MainNavigation.css";
+import Backdrop from "../UIElements/Backdrop";
 
 const MainNavigation = (props) => {
+  const [drawerIsOpen, setdrawerIsOpen] = useState(false);
+
+  const openDrawer = () => {
+    setdrawerIsOpen(true);
+  };
+
+  const closeDrawer = () => {
+    setdrawerIsOpen(false);
+  };
+
   return (
     <React.Fragment>
-      <SideDrawer>
-        <nav className="main-navigation__drawer-nav">
-          <NavLinks />
-        </nav>
-      </SideDrawer>
+      {drawerIsOpen ? <Backdrop onClick={closeDrawer} /> : null}
+      {drawerIsOpen ? (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      ) : null}
+
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
           <span></span>
           <span></span>
           <span></span>
